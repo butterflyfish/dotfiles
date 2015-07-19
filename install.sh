a3ec62a03ec7ac8f75935067eb846d9945c3a457
Michael Zhu <boot2linux@gmail.com>
2015-07-19 23:14:35 +0800

    add comment for install script

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #/bin/sh
 
+# Tmux's configuration file
 ln -sf $PWD/tmux.conf ~/.tmux.conf
 
+# install terminfo that support italic font
+# and they are used by iTerm and Tmux etc
 (
 cd terminfo
 for info in *; do
