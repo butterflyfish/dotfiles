51e65a525fc27f6ae492335a9a048ad3515f8425
Michael Zhu <boot2linux@gmail.com>
2015-07-21 23:49:42 +0800

    add zshrc

--- install.sh
+++ install.sh
@@ -9,9 +9,15 @@ ln -sf $PWD/tmux.conf ~/.tmux.conf
 cd terminfo
 for info in *; do
 	tic $info
+	echo compile terminfo $info
 done
 )
 
 # install fonts
 ./fonts/install.sh
+
+# install oh-my-zsh[https://github.com/robbyrussell/oh-my-zsh]
+[ -d  ~/.oh-my-zsh/.git ] || sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -sf $PWD/zshrc ~/.zshrc
+echo "make link for zshrc"
 
